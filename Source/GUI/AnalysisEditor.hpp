266c805df5f4bff60da93cc9415d5f37cc912d43
Jaiden Muschett <jdm@Jaidens-MacBook-Pro.local>
2017-08-31 13:31:57 +0100

    -a

---
+++ Source/GUI/AnalysisEditor.hpp
@@ -0,0 +1,14 @@
+//
+//  AnalysisEditor.hpp
+//  jd_CMatrix
+//
+//  Created by Jaiden Muschett on 30/08/2017.
+//
+//
+
+#ifndef AnalysisEditor_hpp
+#define AnalysisEditor_hpp
+
+#include <stdio.h>
+
+#endif /* AnalysisEditor_hpp */
